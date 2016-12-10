47332e86cb5e20b1e7da87c9d9b0441f56d6eb1a
Vinay Uday Prabhu <vinayup@gmail.com>
2016-12-10 13:02:25 -0800

    Added timestamp operations

---
+++ Py_utils.py
@@ -0,0 +1,11 @@
+def ts_to_time(ts):
+    # Convert device timestamps to datetime objects
+    utc_dt=datetime.datetime(year=2001,month=1,day=1)+datetime.timedelta(microseconds=np.float(ts))
+    return utc_dt
+######
+# Rounding the device timestamp to the nearest 'T' minute interval
+T_minutes=30
+offset_T=T_minutes*60*1e9  # T minutes in nanoseconds 
+df_beacon['time_rnd1']=pd.to_datetime(((df_beacon.timestamp.apply(ts_to_time).astype(np.int64) // offset_T + 1 ) * offset_T))
+
+
